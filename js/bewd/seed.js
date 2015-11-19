d6260357a8d5fc46f3843a18fbdbae4c8fce8fbf
David Raynes <David.Raynes@teamaol.com>
2015-11-18 19:36:40 -0500

    Default route

--- js/bewd/seed.js
+++ js/bewd/seed.js
@@ -2,6 +2,6 @@
   angular.module('bewd.seed', ['ngRoute'])
     .config(function($routeProvider, $locationProvider) {
       $locationProvider.html5Mode(true);
-      
+      $routeProvider.otherwise('/someSpecialRoute');
     });
 })();
